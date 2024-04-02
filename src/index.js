d8c0dc206c026813b200959455910821f5953439
Sonal Singh <sonal1201kumar@gmail.com>
2024-04-02 11:16:53 +0530

    Feat: Added admin routes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const { PORT } = require('./config/server.config');
 const bodyparser = require('body-parser');
 const connectToDB = require('./config/db.config');
 const router = require('./routes/User.routes');
+const adminRouter = require('./routes/Admin.routes');
 
 const app = express();
 app.use(bodyparser.json());
 
 app.use('/user', router) //user router api -> /signup, /login
-
+app.use('/admin', adminRouter)
 app.listen(PORT, async () => {
     console.log(`listening to port number ${PORT}`)
     await connectToDB();
